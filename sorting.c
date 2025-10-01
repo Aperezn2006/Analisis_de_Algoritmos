5cabf7d89fead5c79cd880ddf0bdeee47a9966cb
Arturo Pérez Noves <62878714+Aperezn2006@users.noreply.github.com>
2025-10-01 19:29:14 +0200

    bubble sort

--- sorting.c
+++ sorting.c
@@ -36,31 +36,24 @@ int InsertSort(int *array, int ip, int iu)
 }
 
 
-
-
 /***************************************************/
 /* Function: SelectSort    Date:                   */
-/* Your comment                                    */
+/* No funciona para arrays vacios                  */
 /***************************************************/
-int BubbleSort(int *array, int ip, int iu)
-{
-  int count_OB = 0, i, j;
+int BubbleSort(int* array, int ip, int iu){
+  int i, j, OB;
 
-  for ( i = ip; i < iu; i++)
-  {
-    
-    for ( j = ip; j < iu - (i - ip); j++)
-    {
-      if (array[j] > array[j + 1])
-      {
-        int aux = array[j];
+  for (i = ip; i < iu ; i++){
+    for (j = 0; j < (iu - i); j++){
+      int temp;
+      if (array[j] > array[j+1]){
+        temp = array[j];
         array[j] = array[j + 1];
-        array[j + 1] = aux;
-        count_OB++;
-      }
+        array[j + 1] = temp;
+        OB++;
+      };
     }
-    
   }
-
-  return count_OB;
+  return OB;
 }
+
