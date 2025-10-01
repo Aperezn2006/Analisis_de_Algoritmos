2c4b39a641eaf38deee9074db21ba472e7d2115e
Penelopez72 <Ismaelnevadoyt@gmail.com>
2025-10-01 19:26:41 +0200

    insert_sort

--- sorting.c
+++ sorting.c
@@ -9,42 +9,58 @@
  *
  */
 
-
 #include "sorting.h"
 
 /***************************************************/
 /* Function: InsertSort    Date:                   */
 /* Your comment                                    */
 /***************************************************/
-int InsertSort(int* array, int ip, int iu)
+int InsertSort(int *array, int ip, int iu)
 {
-  
+  int count_OB = 0, i, j;
+
+  for ( i = ip; i < iu; i++)
+  {
+    
+    for ( j = i; j > 0 && array[j - 1] > array[j]; j--)
+    {
+      int aux = array[j];
+      array[j] = array[j - 1];
+      array[j - 1] = aux;
+      count_OB++;
+    }
+    
+  }
+
+  return count_OB;
 }
+
+
 
 
 /***************************************************/
 /* Function: SelectSort    Date:                   */
-/* No funciona para arrays vacios                  */
+/* Your comment                                    */
 /***************************************************/
-int BubbleSort(int* array, int ip, int iu){
-  int i, j, OB;
+int BubbleSort(int *array, int ip, int iu)
+{
+  int count_OB = 0, i, j;
 
-  for (i = ip; i < iu ; i++){
-    for (j = 0; j < (iu - i); j++){
-      int temp;
-      if (array[j] > array[j+1]){
-        temp = array[j];
+  for ( i = ip; i < iu; i++)
+  {
+    
+    for ( j = ip; j < iu - (i - ip); j++)
+    {
+      if (array[j] > array[j + 1])
+      {
+        int aux = array[j];
         array[j] = array[j + 1];
-        array[j + 1] = temp;
-        OB++;
-      };
+        array[j + 1] = aux;
+        count_OB++;
+      }
     }
+    
   }
-  return OB;
+
+  return count_OB;
 }
-
-
-
-
-
-
